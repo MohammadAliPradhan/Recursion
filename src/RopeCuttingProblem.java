96a5c669b2c7b8f3eecfeb49a48a5ded027894ad
Mohammad Ali <aliiyusuf85@gmail.com>
2023-10-13 15:41:57 +0530

    update

---
+++ src/RopeCuttingProblem.java
@@ -0,0 +1,5 @@
+public class RopeCuttingProblem {
+    public static void main(String[] args) {
+
+    }
+}
--- src/SumOfDigitsUsingRecursionTail.java
+++ src/SumOfDigitsUsingRecursionTail.java
@@ -1,14 +1,16 @@
 public class SumOfDigitsUsingRecursionTail {
     public static int fun(int n)
     {
-        if(n==1)
+        if(n==0)
         {
             return n;
         }
+        int k = n%10;
+        return k+fun(n/10);
 
     }
     public static void main(String[] args) {
-    int answer =     fun(253);
+    int answer =     fun(873);
         System.out.println(answer);
     }
 }
